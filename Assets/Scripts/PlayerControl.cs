da12295c5da8070046a9aec769c8252a19b9471a
Alwyn Tan <alwyntan97@gmail.com>
2017-12-02 17:56:41 -0800

    death fix #2

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -22,6 +22,8 @@ public class PlayerControl : MonoBehaviour {
     void Update() {
         if (!GetComponent<AIScript>().AIEnabled)
         {
+            if (IsDead())
+                PlayerDie();
             // player move if ai is not enabled
             var vert = 0;
             var horiz = 0;
@@ -269,8 +271,6 @@ public class PlayerControl : MonoBehaviour {
             }
             else
                 customTransform.Translate(Vector3.forward);
-            if (IsDead())
-                PlayerDie();
         }
     }
 
@@ -285,8 +285,6 @@ public class PlayerControl : MonoBehaviour {
             }
             else
                 customTransform.Translate(Vector3.back);
-            if (IsDead())
-                PlayerDie();
         }
     }
 
@@ -301,8 +299,6 @@ public class PlayerControl : MonoBehaviour {
             }
             else
                 customTransform.Translate(Vector3.right);
-            if (IsDead())
-                PlayerDie();
         }
     }
 
@@ -317,8 +313,6 @@ public class PlayerControl : MonoBehaviour {
             }
             else
                 customTransform.Translate(Vector3.left);
-            if (IsDead())
-                PlayerDie();
         }
     }
 
